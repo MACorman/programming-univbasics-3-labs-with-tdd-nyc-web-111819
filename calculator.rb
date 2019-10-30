cd91f42b6c75feb4afa31c8318f3a15725e3aa29
Madeline Corman <madelinecorman@gmail.com>
2019-10-30 16:34:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 3
 second_number = 9
-sum = 3 + 9
-difference = 3 - 9
-product = 3 * 9
-quotient = 3 / 9
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
